d1e092dd5b854adb8a1261ba285c1b5b68685a7d
HirsuiteLlama276 <kuba99lol@gmail.com>
2024-10-19 20:12:57 +0200

    Editing user and getting all users

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+const express = require('express');
 const server = express()
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -20,7 +20,6 @@ const userSchema = new mongoose.Schema(
 const DbUser = mongoose.model('Cyb', userSchema);
 
 server.post('/login',  async (req, res) => {
-
     const user = {name: req.body.name, password: req.body.password}
     if (!user){
         return res.status(400).send({error:'Username is required'})
@@ -34,14 +33,14 @@ server.post('/login',  async (req, res) => {
     }
 })
 
-server.post('/pass', async (req,res) =>{
-    const user = {name: req.body.name, password: req.body.password, newPassword: req.body.newPassword}
+server.post('/EditPass', async (req,res) =>{
+    const user = {name: req.body.name, password: req.body.password, passwordNew: req.body.passwordNew}
     if (!user){
         return res.status(400).send({error:'Username is required'})
     }
     try {
         const query = await DbUser.updateOne({name: user.name, password: user.password},
-            {password: user.newPassword})
+            {password: user.passwordNew})
         if (query.matchedCount === 0)
         {
             res.status(404).send({error:"User not found"})
@@ -63,7 +62,33 @@ server.post("/newUser", async (req,res)=>{
         res.status(200).send({message: "User added"})
     }catch (e) {
         console.log(e)
-        res.status(404).send({error:"Internal error"})
+        res.status(500).send({error:"Internal error"})
+    }
+})
+server.post("/editUser", async (req,res)=>{
+    const user = {name: req.body.name, role: req.body.role,
+        nameNew: req.body.nameNew, passwordNew: req.body.passwordNew, roleNew: req.body.roleNew}
+    try{
+        const query = await DbUser.updateOne({name:user.name, role: user.role},
+            {name: user.nameNew,password: user.passwordNew, role: user.roleNew})
+        if (query.modifiedCount === 0)
+        {
+            res.status(404).send({error:"User not found"})
+        }
+        res.status(200).send({message:"User edited!"})
+    }catch (e) {
+        console.log(e)
+        res.status(500).send({error:"Internal error"})
+    }
+})
+
+server.get("/users", async (req,res) =>{
+    try{
+        const query = await DbUser.find({})
+        res.status(200).send({query})
+    }catch (e) {
+        console.log(e)
+        res.status(404).send({error:"Users not found!"})
     }
 })
 
